e654cd961417e0f9c62e23301939b2256415c087
Jack-Tillman <johntillman345@gmail.com>
2023-07-05 12:39:17 -0400

    Started part 2 of project, set up some middleware and starter routes.

---
+++ api/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const usersRouter = express.Router();
+const {getAllUsers} = require('../db');
+
+//apiRouter will try to match /users first, then pass to usersRouter to fire off any middleware
+
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+
+  next();
+});
+//middleware will fire when a GET request is made to /api/users, and return a simple object with an empty array 
+usersRouter.get('/', async (req, res) => {
+    const users = await getAllUsers(); 
+    
+    res.send({
+      users: []
+    });
+  });
+  
+
+module.exports = usersRouter;
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// this is where express app will be 
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+const morgan = require('morgan');
+
+//logs out incoming requests to get the method, route, HTTP response code, and time it takes 
+server.use(morgan('dev'));
+//reads incoming JSON from requests, as long as the request's header has Content-Type: application/json 
+server.use(express.json());
+//body logging
+server.use('/', (req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
+//any request made to a path starting with /api will be handled by apiRouter 
+server.use('/api', apiRouter);
+
+
+
+
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+
+
+
+
+
+
+
+
+
+
 //sudo service postgresql restart
 //psql -U postgres
 // in /etc/postgresql/14/main/ i think
