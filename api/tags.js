be1587e7a9110202c35c40724a54267392ef29ea
Jack-Tillman <johntillman345@gmail.com>
2023-07-09 16:33:23 -0400

    Created route for GET/api/tags/:tagName/posts

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const {getAllTags} = require('../db');
+const {
+  getAllTags,
+  getPostById,
+  getPostsByTagName
+} = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -15,6 +19,28 @@ tagsRouter.get('/', async (req, res) => {
   res.send({
     tags
   });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+
+  try {
+    const taggedPosts = await getPostsByTagName(tagName);
+    // use our method to get posts by tag name from the db
+    if(taggedPosts) {
+      res.send({ posts: taggedPosts});
+    } else {
+      next({
+        name:'NoPosts',
+        message: 'No posts with that tag name were found.'
+      });
+    }
+    // send out an object to the client { posts: // the posts }
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
